88e91976f1b307809a4249515e1b24b4ea339513
Alexey <shanhay90@gmail.com>
2018-09-20 21:55:57 +0300

    11

--- Lesson03/src/main/java/ru.otus.guzev.lesson3/Main.java
+++ Lesson03/src/main/java/ru.otus.guzev.lesson3/Main.java
@@ -5,6 +5,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>(-1);
+        List<String> list = new ArrayList<String>(-1);
     }
 }
