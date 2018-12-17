78871091bf748e44d5ba702d060ad045b71cfab6
Alexey <shanhay90@gmail.com>
2018-12-17 23:24:20 +0300

    Исправление замечаний

--- lesson08/src/main/java/ru/otus/lesson08/Main.java
+++ lesson08/src/main/java/ru/otus/lesson08/Main.java
@@ -3,7 +3,7 @@ package ru.otus.lesson08;
 public class Main {
     public static void main(String[] args)  {
         Department department = new Department(10);
-        System.out.println("Пеовоначальный баланс " + department.getBalance());
+        System.out.println("Первоначальный баланс " + department.getBalance());
         department.getAtm(1).getMoney(10000);
         System.out.println("Баланc департамента после снятия из второго банкомата " +department.getBalance());
         department.backupAllAtm();
@@ -11,13 +11,5 @@ public class Main {
         System.out.println("Баланc департамента после снятия из третьего банкомата " + department.getBalance());
         department.restoreAllAtm();
         System.out.println("Баланc департамента после востановрения из бэкапа " + department.getBalance());
-        department.getAtm(2).getMoney(10000);
-        department.getAtm(3).getMoney(10000);
-        System.out.println(department.getBalance());
-        department.backupAllAtm();
-        department.getAtm(5).getMoney(10000);
-        System.out.println(department.getBalance());
-        department.restoreAllAtm();
-        System.out.println(department.getBalance());
     }
 }
